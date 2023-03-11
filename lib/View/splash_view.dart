2623330098123bc820ea24d815a17753da1ebfd5
nahin <dev.nahin95@gmail.com>
2023-03-11 18:35:16 +0600

    3rd commit

--- lib/View/splash_view.dart
+++ lib/View/splash_view.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:dev_ops/custom_widgets/home.dart';
+import 'package:dev_ops/custom_widgets/splash.dart';
 import 'package:dev_ops/utils/routes/routes_name.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
--- lib/resources/apps_url.dart
+++ lib/resources/apps_url.dart
@@ -1,7 +1,5 @@
 class AppUrl {
 
-
   static String flashCards = 'http://36.255.69.58:5000/api/v1/flashcards/card/category';
-
-  static var allFlashCards = 'http://36.255.69.58:5000/api/v1/flashcards/card/allcards';
+  static String allFlashCards = 'http://36.255.69.58:5000/api/v1/flashcards/card/allcards';
 }
